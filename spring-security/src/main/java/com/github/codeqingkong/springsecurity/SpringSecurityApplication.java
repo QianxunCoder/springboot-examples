30ac5965b0abf650aefa819a048ea4a1761624a5
QingKong <1031556593@qq.com>
2022-11-01 13:06:14 +0800

    project init

---
+++ spring-security/src/main/java/com/github/codeqingkong/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.github.codeqingkong.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author QingKong
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
