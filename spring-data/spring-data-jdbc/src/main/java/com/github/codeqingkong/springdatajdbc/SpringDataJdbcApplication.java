4184f2a84f3683615ec04494eec9f993d002f253
QingKong <1031556593@qq.com>
2023-01-14 20:41:03 +0800

    添加注释信息

--- spring-data/spring-data-jdbc/src/main/java/com/github/codeqingkong/springdatajdbc/SpringDataJdbcApplication.java
+++ spring-data/spring-data-jdbc/src/main/java/com/github/codeqingkong/springdatajdbc/SpringDataJdbcApplication.java
@@ -2,7 +2,10 @@ package com.github.codeqingkong.springdatajdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author QingKong
+ * @date: 2023/1/10
+ */
 @SpringBootApplication
 public class SpringDataJdbcApplication {
 
