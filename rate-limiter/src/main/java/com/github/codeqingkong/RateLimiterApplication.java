8c27cde82d5fb43ab843435d77aad82ea06139d8
QingKong <1031556593@qq.com>
2023-08-04 00:10:55 +0800

    限流模块初始化

--- rate-limiter/src/main/java/com/github/codeqingkong/RateLimiterApplication.java
+++ rate-limiter/src/main/java/com/github/codeqingkong/RateLimiterApplication.java
@@ -3,7 +3,6 @@ package com.github.codeqingkong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 /**
  * @author QingKong
  * @date 2023/08/02
---
+++ rate-limiter/src/main/java/com/github/codeqingkong/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.github.codeqingkong.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kongqing
+ * @date 2023/08/04
+ */
+@RestController
+public class IndexController {
+
+}
