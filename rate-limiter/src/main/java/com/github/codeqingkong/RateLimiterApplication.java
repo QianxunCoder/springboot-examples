a98224f3203969f0bd9b5fc6193049007a68fc55
QingKong <1031556593@qq.com>
2023-08-03 23:08:30 +0800

    限流模块初始化

---
+++ rate-limiter/src/main/java/com/github/codeqingkong/RateLimiterApplication.java
@@ -0,0 +1,16 @@
+package com.github.codeqingkong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author QingKong
+ * @date 2023/08/02
+ */
+@SpringBootApplication
+public class RateLimiterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterApplication.class, args);
+	}
+}
